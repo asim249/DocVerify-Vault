bfbd653c08342a6cb2c628194355405f90ca8835
Asim Mir <asimqamarasim156@gmail.com>
2026-03-16 06:41:11 +0500

    Removed .next from git tracking

--- frontend/.next/dev/static/chunks/app_admin_page_tsx_f1466fbc._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_3a9ff021._.js",
-  "static/chunks/_aae77f9b._.js"
-],
-    source: "dynamic"
-});
